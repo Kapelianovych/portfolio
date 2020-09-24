72fdf059e92490d79216edbc6b419d35a6524042
Kapelianovych Yevhen <kapelianovych.y.v@gmail.com>
2020-09-24 13:01:11 +0300

    Discard public prefix path from route paths.

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import './routes';
 import { Router } from '@prostory/edelweiss';
 import { registerServiceWorker } from './service_worker/service_worker_controller';
 
-Router.to(window.location.pathname);
+Router.to(window.location.pathname.replace('__PUBLIC_PREFIX_PATH__', ''));
 
 /**
  * You may not register service worker, if you do not want to.
