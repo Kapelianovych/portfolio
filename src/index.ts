971c78fccdd13d3cb62c2043f00f19b1cb89fc50
Kapelianovych Yevhen <kapelianovych.y.v@gmail.com>
2020-10-06 00:52:30 +0300

    Enable service worker.

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,4 @@ Router.to(window.location.pathname);
  * You may not register service worker, if you do not want to.
  * Simply delete or comment this line.
  */
-// registerServiceWorker();
+registerServiceWorker();
